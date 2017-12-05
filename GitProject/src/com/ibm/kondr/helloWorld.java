ac0d1a75fe45b726d5bf07af7350262859e992a5
PavelKondratiev <kondratiev@iba.by>
2017-12-05 14:30:02 +0300

    Initial release

---
+++ GitProject/src/com/ibm/kondr/helloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ibm.kondr;
+
+/**
+ * @author KANDRATSYEU_P
+ *
+ */
+public class helloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
